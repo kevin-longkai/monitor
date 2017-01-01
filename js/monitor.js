ae7ac930c3c29acc93546f908717c6a9c8c78177
Kevin <longkai@qianshengqian.com>
2017-01-01 15:49:17 +0800

    新增token判断是否弹出login

--- js/monitor.js
+++ js/monitor.js
@@ -1,13 +1,31 @@
-//判断token是否为空，为空弹出登录
-function checkTokenShowLogin() {
-    var token = YYM.getToken();
-    if (token !== null || token !== undefined || token !== '') {
+/**
+ * 判断token是否为空，为空弹出登录
+ *
+ * @constructor
+ */
+function showLogin() {
+    if (!checkLogin()) {
         YYM.showLogin();
     }
 }
+/**
+ * 检查是否有登录态
+ *
+ * @returns {boolean}
+ */
+function checkLogin() {
+    var flag = true;
+    var token = YYM.getToken();
+    if (token !== null || token !== undefined || token !== '') {
+        flag = false;
+    }
+    return flag;
+}
+
 function getConnectBlueDevice() {
     YYM.connectBluetooth();
 }
+//判断设备是否连接
 function changeButtonStatus(status)
 {
     if (status == 1) {
