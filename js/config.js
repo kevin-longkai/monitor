8c5ddcae05ec8715f66c8de9213da87360ac9ab6
Kevin <longkai@qianshengqian.com>
2017-01-01 16:08:04 +0800

    新增token判断是否弹出login

---
+++ js/config.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Kevin on 1/1/17.
+ */
+/**
+ * 今日动态
+ * @type {string}
+ */
+var today_monitor = "/monitor/monitor-chart/monitor.html";
+function todayMonitor() {
+    window.location.href = today_monitor;
+}
+/**
+ * 首页
+ * @type {string}
+ */
+var shou_ye = "/monitor/monitor/index.html";
+function shouYe() {
+    window.location.href = shou_ye;
+}
+
+/**
+ * 易牙美
+ * @type {string}
+ */
+var yi_ya_mei = "/monitor/monitor-chart/monitor.html";
+function yiYaMei() {
+    window.location.href = yi_ya_mei;
+}
+
+var ask_doctor = "/monitor/doctors/list.html";
+
+function askDoctor() {
+    window.location.href = ask_doctor;
+}
